e2e9b59423d8a38411b4863550c19946cc6d03df
afairlie <ariane.fairlie@gmail.com>
2020-05-26 20:25:44 +0000

    various queries store revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum('annual_revenue')
+puts total_revenue
+
+average_annual = total_revenue / Store.count
+puts average_annual
+
+revenue_over_mil = Store.where('annual_revenue >= 1000000').count
+puts revenue_over_mil
