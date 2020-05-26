f2b87a0a8ddfffb66f7f07fa7b7544c443a02eb5
afairlie <ariane.fairlie@gmail.com>
2020-05-26 21:20:36 +0000

    link employee and store tables many to one relationship, create new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,19 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
+# Employee(id: integer, store_id: integer, first_name: string, last_name: string, hourly_rate: integer, created_at: datetime, updated_at: datetime)
+
 # Your code goes here ...
+@store1.employees.create(first_name: 'Khurram', last_name: 'Virani', hourly_rate: 60)
+
+@store1.employees.create(first_name: 'Stephen', last_name: 'Cat', hourly_rate: 65)
+
+@store1.employees.create(first_name: 'Isabel', last_name: 'Dog', hourly_rate: 68)
+
+@store1.employees.create(first_name: 'Komo', last_name: 'Dog', hourly_rate: 65)
+
+@store2.employees.create(first_name: 'Max', last_name: 'Courteau', hourly_rate: 85)
+
+@store2.employees.create(first_name: 'Ariane', last_name: 'Ingrid', hourly_rate: 85)
+
+@store2.employees.create(first_name: 'Stu', last_name: 'Fairlie', hourly_rate: 85)
